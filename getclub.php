5f3344fe9417a98c00d6f06c8bdb4cf7d0081a71
enriconathaniel <https://enriconathaniel@bitbucket.org/enriconathaniel/skripsienrico.git>
2018-09-04 09:24:02 +0700

    4SeptemberPagi

--- getclub.php
+++ getclub.php
@@ -5,8 +5,6 @@
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Allow-Headers: X-App-Token, Content-Type');
     }
-
-    $id = $_POST['id'];
  
     $conn = new PDO('mysql:host=localhost;dbname=piranha','root','');
     //$conn = new PDO('mysql:host=mysql.idhostinger.com;dbname=u883464978_mone','u883464978_dolbe','Janssen8');
@@ -14,17 +12,18 @@
     //$return = null;
  
 //  $query = $conn->prepare("SELECT artifactID FROM users WHERE username = '$username' LIMIT 1");
-    $data = $conn->query("SELECT id, email, club_info FROM club");
+    $data = $conn->query("SELECT id, email, password, clubinfo FROM klub");
  
-    $club = array();
+    $reward = array();
  
     foreach ($data as $row) {
-        array_push($club, array(
+        array_push($reward, array(
             'id' => $row[0],
             'email' => $row[1],
-            'club_info' => $row[2]
+            'password' => $row[2],
+            'clubinfo' => $row[3]
         ));
     }
  
-    echo json_encode($club);
+    echo json_encode($reward);
 ?>

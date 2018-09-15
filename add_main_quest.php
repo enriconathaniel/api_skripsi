670e89535190498978183cf9a4b644721e290fe9
enriconathaniel <https://enriconathaniel@bitbucket.org/enriconathaniel/skripsienrico.git>
2018-09-16 01:35:24 +0700

    16 September

---
+++ add_main_quest.php
@@ -0,0 +1,42 @@
+<?php
+
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Allow-Headers: X-App-Token, Content-Type');
+    }
+
+    $requestBody = json_decode(file_get_contents('php://input'), true);
+
+    
+
+    $id_atlet = $requestBody['id_atlet'];
+    $id_gaya = $requestBody['id_gaya'];
+    $waktu_target = $requestBody['waktu_target'];
+ 
+    include 'config/db_con.php';
+    $query = "INSERT INTO main_quest(id_atlet, id_gaya, target) VALUES('".$id_atlet."','".$id_gaya."','".$waktu_target."')";
+
+    $data = $conn -> query($query);
+    $return = null;
+ 
+    // $query = $conn->prepare("SELECT id FROM users WHERE email = '$email' LIMIT 1");
+    // $query->execute();
+    // $row = $query->fetch();
+ 
+    // $userID = $row[0];
+ 
+    
+ 
+    if($data) {
+        $return['insert'] = true;   
+    }
+    else {
+        $return['insert'] = false;
+    }
+ 
+    echo json_encode($return);
+
+    
+    
+?>
--- getquest.php
+++ getquest.php
@@ -5,32 +5,27 @@
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Allow-Headers: X-App-Token, Content-Type');
     }
-    
+    $requestBody = json_decode(file_get_contents('php://input'), true);
+    $id = $requestBody['id'];
     include 'config/db_con.php';
     //$conn = new PDO('mysql:host=localhost;dbname=piranha','root','');
     //$conn = new PDO('mysql:host=mysql.idhostinger.com;dbname=u883464978_mone','u883464978_dolbe','Janssen8');
- 
+    
     //$return = null;
  
 //  $query = $conn->prepare("SELECT artifactID FROM users WHERE username = '$username' LIMIT 1");
     //$data = $conn->query("SELECT id, deskripsi, point, max_umur, min_exp, max_exp, repetition FROM quest");
     
-    $data = $conn->query("SELECT quest.id, quest.deskripsi, quest.point, quest.max_umur, quest.min_exp, quest.max_exp, quest.repetition, gaya_renang.nama_gaya, gaya_renang.jarak, quest.waktu_target FROM quest JOIN gaya_renang ON quest.id_gaya = gaya_renang.id");
+    $data = $conn->query("SELECT main_quest.id, gaya_renang.nama_gaya, gaya_renang.jarak, main_quest.target FROM main_quest JOIN gaya_renang ON main_quest.id_gaya = gaya_renang.id WHERE main_quest.id_atlet = '".$id."'");
     
     $quest = array();
  
     foreach ($data as $row) {
         array_push($quest, array(
             'id' => $row[0],
-            'deskripsi' => $row[1],
-            'point' => $row[2],
-            'max_umur' => $row[3],
-            'min_exp' => $row[4],
-            'max_exp' => $row[5],
-            'repetition' => $row[6],
-            'gaya_renang_nama' => $row[7],
-            'gaya_renang_jarak' => $row[8],
-            'quest_waktu_target' => $row[9]
+            'gaya_renang_nama' => $row[1],
+            'gaya_renang_jarak' => $row[2],
+            'quest_waktu_target' => $row[3]
         ));
     }
  
